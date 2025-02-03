16fdd820c8c11f9cc8c1a6eac685e982e5a72151
Johann Christian M. Hernandez <johannhernandez1233@gmail.com>
2025-02-03 13:25:09 +0800

    html file conversion to blade and route testing

--- resources/views/hatchery/main_module.blade.php
+++ resources/views/hatchery/main_module.blade.php
@@ -86,12 +86,12 @@
         <div class="card">
             <img src="/Images/Egg_Add.png" alt="">
             <h3>EGG COLLECTION <br> ENTRY</h3>
-            <button onclick="window.location.href='/Html/egg_collection.html'">OPEN</button>
+            <button onclick="window.location.href='/egg-collection-entry'">OPEN</button>
         </div>
         <div class="card">
             <img src="/Images/Egg_Temp.png" alt="">
             <h3>EGG TEMPERATURE CHECK <br> ENTRY</h3>
-            <button onclick="window.location.href='/Html/egg_temp.html'">OPEN</button>
+            <button onclick="window.location.href='/egg-temperature-check-entry'">OPEN</button>
         </div>
         <div class="card">
             <img src="/Images/Egg_Reject.png" alt="">
