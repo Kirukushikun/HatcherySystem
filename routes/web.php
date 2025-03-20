3c95d5d4d7c8c5adfc8cb4fc7232cf91c6a207aa
Iverson Craig <148548092+Kirukushikun@users.noreply.github.com>
2025-03-20 10:13:28 +0800

    Revert "Updated master database"

--- routes/web.php
+++ routes/web.php
@@ -175,8 +175,6 @@ Route::get('/test', function () {
 	return view('hatchery.report_module');
 });
 
-// Master Database ---------
-  
 Route::get('/fetch-master-database-data', [MasterDatabaseTable::class, 'fetchData'])->name('master.database.fetch');
 
 Route::get('/master-database', [MasterDatabaseController::class, 'master_database_index'])->name('master.database.index');
