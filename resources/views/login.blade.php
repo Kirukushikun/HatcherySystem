1e9ef0fb56d8de85cccb46afea9e5c245883148a
Iverson Craig <148548092+Kirukushikun@users.noreply.github.com>
2025-05-05 11:43:22 +0800

    modified:   resources/views/login.blade.php

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -6,4 +6,12 @@
 
 @section('content')
 	<h3>Login Using BGC Authenticator. :)</h3>
+	<script>
+		function redirectAfter3Seconds() {
+			setTimeout(function() {
+				window.location.href = '/home';
+			}, 2000); // 3000 milliseconds = 3 seconds
+		}
+		redirectAfter3Seconds();
+	</script>
 @endsection
